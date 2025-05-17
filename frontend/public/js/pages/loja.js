a476936e84d87f61a9bcbc534be20588e4346129
DouglasMoraesDev <doougmooraes2024@gmail.com>
2025-05-16 22:51:09 -0300

    commit 5

--- frontend/public/js/pages/loja.js
+++ frontend/public/js/pages/loja.js
@@ -8,14 +8,16 @@ export function lojaPage() {
     async init() {
       const div = document.getElementById('loja');
       const res = await api.getMercadorias();
+
       div.innerHTML = res.data.map(m => `
         <div class="card">
           <h4>${m.nome}</h4>
           <p>R$${m.valorUnitario.toFixed(2)}</p>
-          ${m.fotos.map(f => `<img src="/uploads/${f.caminho}" width="80">`).join('')}
+          ${m.fotos.map(f => `<img src="${f.caminho}" width="80">`).join('')}
           <button data-id="${m.id}">Conversar</button>
         </div>
       `).join('');
+
       div.querySelectorAll('button').forEach(btn => {
         btn.onclick = () => {
           const m = res.data.find(x => x.id == btn.dataset.id);
